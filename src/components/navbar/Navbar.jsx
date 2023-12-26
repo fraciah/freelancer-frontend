dc034de39d0119e268389cd6d5f2dcb657f89643
fraciah <karfraciah@gmail.com>
2023-12-26 12:23:16 +0300

    Add user info

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,40 +2,42 @@ import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import gigitise from '../../../public/gigitise.svg';
 import { IoMdNotificationsOutline } from 'react-icons/io';
+import { useAuthContext } from '../../providers/AuthProvider';
 import './navbar.css';
 
 const Navbar = () => {
-  const iconSize = 28;
-  const [username, setUsername] = useState('Username');
-  const [profilePhoto, setProfilePhoto] = useState(null);
+    const iconSize = 28;
+    const { loadingUserProfile, loadedUserProfile, handleLogOut } = useAuthContext();
+    const [userProfile, setUserProfile] = useState(loadedUserProfile);
 
-  const navigate = useNavigate();
 
-  return (
-    <div className="nav">
-        <div style={{ cursor: 'pointer' }} className="heading-container">
-            <img src={gigitise} className="logo-img" alt="gigitise-img" />
-            <h2>Gigitise</h2>
+    const navigate = useNavigate();
+
+    return(
+        <div className="nav">
+            <div className="heading-container">
+                <img src={gigitise} className="logo-img" alt="gigitise-logo" />
+                <h2>Gigitise</h2>
+            </div>
+            <div className="search-nav">
+                <input className="search-input" type="text" placeholder="Search categories of orders" />
+            </div>
+            <div>
+                <h2 className="logout">Logout</h2>
+            </div>
+            <div className="notif-bell">
+                <IoMdNotificationsOutline className="notif-icon" size={iconSize} />
+            </div>
+            <div className="profile-info" onClick={() => navigate('./profile')}>
+                <article style={{width: loadingUserProfile?'3rem':''}}>{userProfile?.username}</article>
+                {
+                userProfile?.profile_photo?
+                <img src={userProfile?.profile_photo} alt="profile cover" />:
+                <article className='img-placeholder'>{userProfile && `${(userProfile?.username?.charAt(0)?.toUpperCase() + userProfile?.username.slice(1).slice(0,1))}`}</article>
+                }
+            </div>
         </div>
-        <div className="search-nav">
-            <input className="search-input" type="text" placeholder="Search categories of orders" />
-        </div>
-        <div>
-            <h2 className="logout">Logout</h2>
-        </div>
-        <div className="notif-bell">
-            <IoMdNotificationsOutline className="notif-icon" size={iconSize} />
-        </div>
-        <div className="profile" onClick={() => navigate('./profile')}>
-            <article>{username}</article>
-            {profilePhoto ? (
-                <img src={profilePhoto} alt="ProfileImg" />
-            ) : (
-                <article className="img-placeholder">{username.slice(0, 1).toUpperCase()}{username.slice(1, 2).toLowerCase()}</article>
-            )}
-        </div>
-    </div>
-  );
+    );
 };
 
 export default Navbar;
