87cb212410a2b14943c9801be506c316cb655175
Musya Joseph <57936020+JosephMusya@users.noreply.github.com>
2024-01-20 23:19:01 +0300

    Revert "Bidding"

--- src/components/order-component/OrderComponent.jsx
+++ src/components/order-component/OrderComponent.jsx
@@ -2,64 +2,76 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { checkDeadline, formatDeadline } from '../../../utils/helpers/DeadlineFormat';
 
+ 
+
 const OrderComponent = ({ content }) => {
-  const navigate = useNavigate();
-  const orderId = content.id;
+    const navigate = useNavigate();
+    const orderId = content.id;
 
-  const deadline = formatDeadline(content.deadline);
-  const deadlinePassed = checkDeadline(content.deadline);
+    const deadline = formatDeadline(content.deadline);
+    const deadlinePassed = checkDeadline(content.deadline);
 
-  const status = content.status;
+    const status = content.status;
 
-  const maxCharsToDisplay = 70;
+    const maxCharsToDisplay = 70;
 
-  return (
-    <div className='order-content' onClick={() => navigate(`../order/${orderId}?status=${status.toLowerCase()}`)}>
-      <div className='title-box'>
-        <article>
-          {(content.title.length > maxCharsToDisplay) ? `${(content.title).slice(0, 60)}...` : content.title}
-        </article>
-      </div>
-      
-      <div className='bottom-box '>
-        <div className='fx-start'>
+  ;
+
+    return (
+        <div className='order-content' onClick={() => navigate(`../order/${orderId}`)}>
+            <div className='title-box'>
+                <article>
+                    {
+                        (content.title.length > maxCharsToDisplay) ? `${(content.title).slice(0, 60)}...` :
+                            content.title
+                    }
+                </article>
+            </div>
+            
+            <div className='bottom-box '>
+                
+                <div className='fx-start'>
+                
+                </div>
+                {
+            content.status === 'Available' &&(
+                <div></div>
+            )
+                             
+            }
+                
+                </div>     
+           
+
+            <div className='bottom-box'>
+                <div className='fx-start'>
+                    <article>{content.category}</article>
+                    <article>${content.amount}</article>
+                </div>
+                {status === 'Completed' ?
+                    <div className='fx-end'>
+                        <article className='deadline'>Completed</article>
+                    </div> :
+                    <div className='fx-end'>
+                        {deadlinePassed && (
+                            <article className='deadline' style={{
+                                color: 'red',
+                                backgroundColor: '#f7fafc'
+                            }}>
+                                {deadline}
+                                <span className='ml-2'>overdue</span>
+                            </article>
+                        )}
+                        {!deadlinePassed && (
+                            <article className='deadline'>
+                                {deadline}
+                            </article>
+                        )}
+                    </div>
+                }
+            </div>
         </div>
-        {content.status === 'Available' && (
-          <div></div>
-        )}
-      </div>   
-      
-        
-
-      <div className='bottom-box'>
-        <div className='fx-start'>
-          <article>{content.category}</article>
-          <article>${content.amount}</article>
-        </div>
-        {status === 'Completed' ?
-          <div className='fx-end'>
-            <article className='deadline'>Completed</article>
-          </div> :
-          <div className='fx-end'>
-            {deadlinePassed && (
-              <article className='deadline' style={{
-                color: 'red',
-                backgroundColor: '#f7fafc'
-              }}>
-                {deadline}
-                <span className='ml-2'>overdue</span>
-              </article>
-            )}
-            {!deadlinePassed && (
-              <article className='deadline'>
-                {deadline}
-              </article>
-            )}
-          </div>
-        }
-      </div>
-    </div>
-  );
+    );
 }
 
 export default OrderComponent;
