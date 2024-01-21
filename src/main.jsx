09ef6c20a51624b74940cd2e3ba2872b2d2b6dd2
josephmusya <muciajoe@gmail.com>
2024-01-21 13:46:02 +0300

    Remove strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,25 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom';
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { AuthProvider } from './providers/AuthProvider.jsx'
-import ToasterContext from './components/toasterContext.jsx'
-import { ChatProvider } from './providers/ChatProvider.jsx'
-import { OrderProvider } from './providers/OrderProvider.jsx'
-import { NotificationProvider } from './providers/NotificationProvider.jsx';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { AuthProvider } from "./providers/AuthProvider.jsx";
+import ToasterContext from "./components/toasterContext.jsx";
+import { ChatProvider } from "./providers/ChatProvider.jsx";
+import { OrderProvider } from "./providers/OrderProvider.jsx";
+import { NotificationProvider } from "./providers/NotificationProvider.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <AuthProvider>
-    <OrderProvider>
-      <ChatProvider>
-    <NotificationProvider>
-    <ToasterContext/>
-        <App />
-    </NotificationProvider>
-    </ChatProvider>
-    </OrderProvider>
+      <OrderProvider>
+        <ChatProvider>
+          <NotificationProvider>
+            <ToasterContext />
+            <App />
+          </NotificationProvider>
+        </ChatProvider>
+      </OrderProvider>
     </AuthProvider>
   </BrowserRouter>
-  </React.StrictMode>,
-)
+);
