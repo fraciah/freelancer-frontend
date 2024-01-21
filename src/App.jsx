ec619a8c8f0abf91270a875bac996bfc63f77ed8
josephmusya <muciajoe@gmail.com>
2024-01-21 15:59:13 +0300

    Modify url name

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,68 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Dashboard from './pages/dashboard/Dashboard';
-import Login from './pages/login/Login';
-import { useAuthContext } from './providers/AuthProvider';
-import { useState } from 'react';
-import PasswordReset from './pages/reset-password/PasswordReset';
-import SetPassword from './components/modal/set-password/SetPassword';
-import BadToken from './pages/bad-token/BadToken';
-import ExpiredToken from './pages/expired-token/ExpiredToken';
-import Navbar from './components/navbar/Navbar';
-import SideNav from './components/sidenav/SideNav';
-import Completed from './pages/orders/completed/Completed';
-import InProgress from './pages/orders/in-progress/InProgress';
-import Profile from './pages/profile/Profile';
-import Notification from './pages/notification/Notification';
-import OrderView from './pages/orders/order-view/OrderView';
-import Settings from './pages/settings/Settings';
-import Available from './pages/orders/available/Available';
-import BidDetails from './pages/orders/Bid-view/BidDetails';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Login from "./pages/login/Login";
+import { useAuthContext } from "./providers/AuthProvider";
+import { useState } from "react";
+import PasswordReset from "./pages/reset-password/PasswordReset";
+import SetPassword from "./components/modal/set-password/SetPassword";
+import BadToken from "./pages/bad-token/BadToken";
+import ExpiredToken from "./pages/expired-token/ExpiredToken";
+import Navbar from "./components/navbar/Navbar";
+import SideNav from "./components/sidenav/SideNav";
+import Completed from "./pages/orders/completed/Completed";
+import InProgress from "./pages/orders/in-progress/InProgress";
+import Profile from "./pages/profile/Profile";
+import Notification from "./pages/notification/Notification";
+import OrderView from "./pages/orders/order-view/OrderView";
+import Settings from "./pages/settings/Settings";
+import Available from "./pages/orders/available/Available";
+import BidDetails from "./pages/orders/Bid-view/BidDetails";
 
 function App() {
   const { userToken } = useAuthContext();
 
-  const Main = ()  => {
-
+  const Main = () => {
     return (
       <>
         <main className="app">
           <SideNav />
-          <div className='app-main-content'>
+          <div className="app-main-content">
             <Navbar />
-            <div className='routes'>
+            <div className="routes">
               <Routes>
-                <Route path='/' element={<Dashboard />}/> 
-                <Route path='/in-progress' element={<InProgress/>}/>
-                <Route path='/available' element={<Available/>} />
-                <Route path='/Bid-view' element={<BidDetails />} />  
-                <Route path='/completed'element={<Completed/>}/>
-                <Route path='/profile'element={<Profile/>}/>
-                <Route path='/notifications'element={<Notification/>}/>
-                <Route path='/settings'element={<Settings/>}/>
-                <Route path='/order/:orderId' element={<OrderView/>}/>
-              </Routes> 
-            </div>   
-          </div>                  
-        </main>       
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/in-progress" element={<InProgress />} />
+                <Route path="/available" element={<Available />} />
+                <Route path="/my-bids" element={<BidDetails />} />
+                <Route path="/completed" element={<Completed />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/notifications" element={<Notification />} />
+                <Route path="/settings" element={<Settings />} />
+                <Route path="/order/:orderId" element={<OrderView />} />
+              </Routes>
+            </div>
+          </div>
+        </main>
       </>
-    )
-  }
+    );
+  };
 
   return (
     <>
       <Routes>
-        <Route path='login' element={<Login/>}/>
-        <Route path='/app/*' element={userToken?<Main/>:<Login/>}/>
-        <Route path='reset-password' element={<PasswordReset/>}/>
-        <Route path='/used-token/:uidb64/:token/' element={<ExpiredToken/>} />
-        <Route path='/bad-token/:uidb64/:token/' element={<BadToken/>} />
-        <Route path='/set-new-password/:uidb64/:token/' element={<SetPassword/>}/>
+        <Route path="login" element={<Login />} />
+        <Route path="/app/*" element={userToken ? <Main /> : <Login />} />
+        <Route path="reset-password" element={<PasswordReset />} />
+        <Route path="/used-token/:uidb64/:token/" element={<ExpiredToken />} />
+        <Route path="/bad-token/:uidb64/:token/" element={<BadToken />} />
+        <Route
+          path="/set-new-password/:uidb64/:token/"
+          element={<SetPassword />}
+        />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/sidenav/SideNav.jsx
+++ src/components/sidenav/SideNav.jsx
@@ -1,48 +1,57 @@
-import React from 'react';
-import './sidenav.css';
+import React from "react";
+import "./sidenav.css";
 // import gigitise from '../../../../public/gigitise.svg';
 import { useNavigate } from "react-router-dom";
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { MdAdd, MdTaskAlt, MdPendingActions, MdAccessTime } from 'react-icons/md';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  MdAdd,
+  MdTaskAlt,
+  MdPendingActions,
+  MdAccessTime,
+} from "react-icons/md";
 import { FiMenu } from "react-icons/fi";
 import { FaClockRotateLeft } from "react-icons/fa6";
 
 const SideNav = () => {
-    const navigate = useNavigate();   
-    const iconSize = 25;
+  const navigate = useNavigate();
+  const iconSize = 25;
 
-    return (
-        <div className='side-nav'>
-            <h1 style={{cursor:'pointer'}} className='heading-logo'  onClick={()=>navigate('./')}>
-                {/* <img src={gigitise} style={{width:'3rem', color:'white'}} alt="" /> */}
-                Gigitise
-            </h1>
-            <div className='actions'>
-                <Link to='/app' className="nav-item ">
-                    <FiMenu size={iconSize}/>
-                    Dashboard
-                </Link>
-                <Link to='./available' className="nav-item">
-                <FaClockRotateLeft size={iconSize}/>
-                   Available
-                </Link>
-                <Link to='./Bid-view' className="nav-item">
-                <MdAccessTime  size={iconSize}/>
-                    My Bids
-                </Link>
-                <Link to='./in-progress' className="nav-item">
-                    <MdPendingActions size={iconSize}/>
-                    In Progress
-                </Link>
-                
-                <Link to='./completed' className="nav-item">
-                    <MdTaskAlt size={iconSize}/>
-                    Completed
-                </Link>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="side-nav">
+      <h1
+        style={{ cursor: "pointer" }}
+        className="heading-logo"
+        onClick={() => navigate("./")}
+      >
+        {/* <img src={gigitise} style={{width:'3rem', color:'white'}} alt="" /> */}
+        Gigitise
+      </h1>
+      <div className="actions">
+        <Link to="/app" className="nav-item ">
+          <FiMenu size={iconSize} />
+          Dashboard
+        </Link>
+        <Link to="./available" className="nav-item">
+          <FaClockRotateLeft size={iconSize} />
+          Available
+        </Link>
+        <Link to="./my-bids" className="nav-item">
+          <MdAccessTime size={iconSize} />
+          My Bids
+        </Link>
+        <Link to="./in-progress" className="nav-item">
+          <MdPendingActions size={iconSize} />
+          In Progress
+        </Link>
+
+        <Link to="./completed" className="nav-item">
+          <MdTaskAlt size={iconSize} />
+          Completed
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default SideNav;
