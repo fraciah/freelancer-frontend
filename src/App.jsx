a39df48a30999fe4d95679be139c0848913cf8fa
josephmusya <muciajoe@gmail.com>
2024-02-23 01:54:10 +0300

    Add: Dynamic data

--- src/App.jsx
+++ src/App.jsx
@@ -20,12 +20,12 @@ import Settings from "./pages/settings/Settings";
 import Available from "./pages/orders/available/Available";
 import BidDetails from "./pages/orders/Bid-view/BidDetails";
 
-export const ThemeContext = createContext(null)
+export const ThemeContext = createContext(null);
 
 function App() {
   const [theme, setTheme] = useState("dark");
   const { userToken } = useAuthContext();
-  
+
   const toggleTheme = () => {
     setTheme((curr) => (curr === "light" ? "dark" : "light"));
   };
@@ -33,47 +33,53 @@ function App() {
   const Main = () => {
     return (
       <>
-      <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      <main className="app" id={theme}>
-          <SideNav />
-          <div className="app-main-content">
-            <Navbar />
-            <div className="routes">
-              <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/in-progress" element={<InProgress />} />
-                <Route path="/available" element={<Available />} />
-                <Route path="/my-bids" element={<BidDetails />} />
-                <Route path="/completed" element={<Completed />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/notifications" element={<Notification />} />
-                <Route path="/client-profile" element={<ClientProfile />} />
-                <Route path="/settings" element={<Settings />} />
-                <Route path="/order/:orderId" element={<OrderView />} />
-              </Routes>
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+          <main className="app" id={theme}>
+            <SideNav />
+            <div className="app-main-content">
+              <Navbar />
+              <div className="routes">
+                <Routes>
+                  <Route path="/" element={<Dashboard />} />
+                  <Route path="/in-progress" element={<InProgress />} />
+                  <Route path="/available" element={<Available />} />
+                  <Route path="/my-bids" element={<BidDetails />} />
+                  <Route path="/completed" element={<Completed />} />
+                  <Route path="/profile" element={<Profile />} />
+                  <Route path="/notifications" element={<Notification />} />
+                  <Route
+                    path="/client-profile/:clientParam"
+                    element={<ClientProfile />}
+                  />
+                  <Route path="/settings" element={<Settings />} />
+                  <Route path="/order/:orderId" element={<OrderView />} />
+                </Routes>
+              </div>
             </div>
-          </div>
-        </main>
-      </ThemeContext.Provider>
+          </main>
+        </ThemeContext.Provider>
       </>
     );
   };
 
   return (
     <>
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
-    <Routes>
-        <Route path="login" element={<Login />} />
-        <Route path="/app/*" element={userToken ? <Main /> : <Login />} />
-        <Route path="reset-password" element={<PasswordReset />} />
-        <Route path="/used-token/:uidb64/:token/" element={<ExpiredToken />} />
-        <Route path="/bad-token/:uidb64/:token/" element={<BadToken />} />
-        <Route
-          path="/set-new-password/:uidb64/:token/"
-          element={<SetPassword />}
-        />
-      </Routes>
-    </ThemeContext.Provider>
+      <ThemeContext.Provider value={{ theme, toggleTheme }}>
+        <Routes>
+          <Route path="login" element={<Login />} />
+          <Route path="/app/*" element={userToken ? <Main /> : <Login />} />
+          <Route path="reset-password" element={<PasswordReset />} />
+          <Route
+            path="/used-token/:uidb64/:token/"
+            element={<ExpiredToken />}
+          />
+          <Route path="/bad-token/:uidb64/:token/" element={<BadToken />} />
+          <Route
+            path="/set-new-password/:uidb64/:token/"
+            element={<SetPassword />}
+          />
+        </Routes>
+      </ThemeContext.Provider>
     </>
   );
 }
